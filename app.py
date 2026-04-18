7f3efae39b51e90861100bac904cb9e716bb89b4
logiflydevelopers-cloud <logiflydevelopers@gmail.com>
2026-04-18 13:12:59 +0530

    Update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from routes.chat_routes import router as chat_router
-from routes.my_ai_routes import router as my_ai_router
 
 app = FastAPI(
     title="AI Girlfriend API",
@@ -8,7 +7,6 @@ app = FastAPI(
 )
 
 app.include_router(chat_router)
-app.include_router(my_ai_router)
 
 
 @app.get("/")
