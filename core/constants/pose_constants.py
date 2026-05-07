fc697d6f11ee2d3acfbddc6ce0c358b79a7e0ff9
logiflydevelopers-cloud <logiflydevelopers@gmail.com>
2026-05-07 15:41:18 +0530

    Updated Pose Routes

--- core/constants/pose_constants.py
+++ core/constants/pose_constants.py
@@ -25,9 +25,86 @@ POSES = {
         "Show me you squatting with both arms lifted above, eyes gently closed, serene and sensual mood, soft lighting",
         "Show me you squatting casually with one hand raised in the air, relaxed wrist, urban streetwear vibe, confident attitude",
     ],
-    "ass": [
+    "boobs": [
         "Show me your naked breasts, with your hands above your head",
-        "Show me you squatting with both arms lifted above, eyes gently closed, serene and sensual mood, soft lighting",
-        "Show me you squatting casually with one hand raised in the air, relaxed wrist, urban streetwear vibe, confident attitude",
-    ]
+        "Show me you topless, sitting down and looking sultry",
+        "Show me your bare breasts, while looking seductive",
+    ],
+    "ass": [
+        "Show me a nude, of your ass",
+        "Show me you on all fours grabbing your naked ass from behind",
+        "Show me your naked ass, standing from behind"
+    ],
+    "blowjob": [
+        "Show me you performing oral sex",
+        "Show me a blowjob",
+        "Show me a man standing, side view, blow job"
+    ],
+    "cumshot": [
+        "Show me a blowjob cumshot",
+        "Show me your face covered in cum, cumshot facial",
+        "Show me your boobs covered in cum, cumshot"
+    ],
+    "pussy": [
+        "Show me a close up of your vagina",
+        "Show me your vagina, legs spread",
+        "Show me you spreading your pussy"
+    ],
+    "cowgirl": [
+        "Show me you getting fucked in the cowgirl position, view from the front",
+        "Show me you in the cowgirl position getting fucked in the ass, cumshot",
+        "Show me you in the cowgirl position getting fucked in the pussy, cumshot",
+        "Show me a front view of you having sex in the cowgirl position, with you on top, excited"
+    ],
+    "feet": [
+        "Show me you giving a foot job to a man with a penis and a larger amount of ejaculate",
+        "Show me a view of your feet, while you are naked",
+        "Show me a view of your feet covered in cum, while naked"
+    ],
+    "doggystyle": [
+        "Show me you having doggy style sex on all fours with a man who is fucking your pussy",
+        "Show me doggystyle sex position",
+        "Show me doggy style, fucked from behind"
+    ],
+    "masturbation": [
+        "Show me you masturbating, pussy spread, fingering pussy",
+        "Show me you rubbing your pussy",
+        "Show me your legs spread, fingering your pussy"
+    ],
+    "missionary": [
+        "Show me you having missionary sex with your legs in the air, orgasmic facial expression",
+        "Show me you getting fucked in the missionary position, with your legs spread wide",
+        "Show me you having missionary sex and holding your legs"
+    ],
+    "dildo": [
+        "Show me you inserting dildo into you pussy, with the focused on your vagina, you are masturbating, with a facial expression of an orgasm",
+        "Show me you naked playing with a sex toy",
+        "Show me you putting a dildo in your pussy, look of pleasure on your face"
+    ],
+    "bondage": [
+        "Show me you tied up in bondage ropes, naked",
+        "Show me you tied up from the front with your pussy exposed",
+        "Show me you tied up, bondage ropes, naked, legs up"
+    ],
+    "handjob": [
+        "Show me a side view of a handjob, with a look of pure excitement on your face with your mouth open",
+        "Show me a handjob, with a surprised facial expression, covered in semen",
+        "Show me a handjob, with you covered in cum"
+    ],
+    "squirting": [
+        "Show me you squirting, mouth open",
+        "Show me you squirting with your legs spread open",
+        "Show me you squirting"
+    ],
+    "boobjob": [
+        "Show me you with a man penis between your breasts, your mouth is open",
+        "Show me you with a man penis between your breasts as your mouth is open and you look excited",
+        "Show me you giving a boobjob from my POV"
+    ],
+    "reverse cowgirl": [
+        "Show me you in reverse cowgirl, with a penis in your pussy",
+        "Show me you getting fucked in reverse cowgirl, as your look back and have an orgasm",
+        "Show me you getting fucked in reverse cowgirl, with cum dripping from your pussy",
+        "Show me reverse cowgirl, with penis in your ass"
+    ],
 }
