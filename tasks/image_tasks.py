68b1ff030060048d1e46d6dd0a09ccce834b4b83
logiflydevelopers-cloud <logiflydevelopers@gmail.com>
2026-05-08 16:13:23 +0530

    Updated Generate Image API to accept the style

--- tasks/image_tasks.py
+++ tasks/image_tasks.py
@@ -1,11 +1,20 @@
 from core.celery_app import celery_app
 from services.character_service import generate_image_from_prompt
 
+@celery_app.task(bind=True)
+def generate_image_task(
+    self,
+    character_id,
+    user_id,
+    prompt,
+    style="realistic",
+    num_images=1
+):
 
-@celery_app.task
-def generate_image_task(character_id, user_id, prompt):
     return generate_image_from_prompt(
         character_id=character_id,
         user_id=user_id,
-        prompt=prompt
+        prompt=prompt,
+        style=style,
+        num_images=num_images
     )
